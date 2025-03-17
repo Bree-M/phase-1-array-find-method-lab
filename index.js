3966a420b0404ab405ce90095a60cba1b30e7b36
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-17 14:18:22 +0300

    updated changes in arrays

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function superbowlWin(records) {
     let win = records.find(record => record.result === "W");
-    return win ? win.Year : undefined;
+    return win ? `Denver Broncos won in ${win.Year}` : "Denver Broncos never won";
 }
 
-const denverBroncosFootballTeam = [
+const records = [
 
     {Year:"2015", result:"W"},
     {Year: "2014" ,result:"N/A" },
     { Year: "2013", result: "L"},
 ];
 
-console.log(superbowlWin(denverBroncosFootballTeam));
+console.log(superbowlWin(records));
