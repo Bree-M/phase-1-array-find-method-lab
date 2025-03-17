c202e3d0257b22c4e03284596ea6e8fd000299fc
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-17 14:51:24 +0300

    last commit to my arrays lab

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function superbowlWin(records) {
     let win = records.find(record => record.result === "W");
-    return win ? win.Year : Undefined;
+    return win ? `Denver Broncos won in undefined undefined ${win.Year}` : "Undefined";
 }
 
 const records = [
 
     {Year:"2015", result:"W"},
     {Year: "2014" ,result:"N/A" },
-    { Year: "2013", result: "L"},
+    { Year: "2013", result: "L"}
 ];
 
 console.log(superbowlWin(records));
