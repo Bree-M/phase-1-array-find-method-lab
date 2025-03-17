68e1d4e8b5f963fc3bf690a944bbc35b52ba3e7c
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-17 12:59:20 +0300

    first commit in arrays

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(records) {
+    let win = records.find(record => record.result === "W");
+    return win ? win.Year : undefined;
+}
+
+const superbowlRecords = [
+
+    {Year:"2015", result:"W"},
+    {Year: "2014" ,result:"N/A" },
+    { Year: "2013", result: "L"},
+];
+
+console.log(superbowlWin(superbowlRecords));
