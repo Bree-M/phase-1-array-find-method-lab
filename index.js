3a3ed17afc09c7180c8cdde27a65923c00605acc
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-19 13:30:54 +0300

    commit my chnages

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function superbowlWin(records) {
     let win = records.find(game => game.result === "Win");
-    return win ? win.Year : "undefined";
+    return win ? win.Year : undefined;
 }
 
 const records = [
 
-    {Year:"2015", result:"Win"},
-    {Year: "2014" ,result:"N/A" },
-    { Year: "2013", result: "L"}
+    {Year:2015, result: "Win"},
+    {Year: 2014 ,result:"N/A" },
+    { Year: 2013, result: "L"}
 ];
 
 console.log(superbowlWin(records));
 
 const noWinRecorded = [
-    { Year: "2014", result: "N/A" },
-    { Year: "2013", result: "L" }
+    { Year: 2014, result: "N/A"},
+    { Year: 2013, result: "L" }
 
 ];
 
