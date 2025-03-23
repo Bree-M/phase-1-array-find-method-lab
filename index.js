a18914fec3c55212ce9db6fe584f90b5ce8b6995
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 15:41:35 +0300

    required labs update

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const record = [
-    { year: "2015", result: "W" },
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L" }
-];
-
 function superbowlWin(record) {
     const win = record.find(game => game.result === "W");
     return win ? win.year : undefined;
 }
 
-console.log(superbowlWin(record));
-console.log(superbowlWin([{year: "2020", result:"L"}]));
+const record1 = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L" }
+];
+
+const record2 = [
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L" }
+];
+
+console.log(superbowlWin(record1));
+console.log(superbowlWin(record2));
+
+
+
 
 
 
